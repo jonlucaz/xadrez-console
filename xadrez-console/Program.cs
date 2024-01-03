a18aeb56a4e4aec9631a17938aec6d7269c3bfcf
Jonathan Lucas <jonathanlucastri@gmail.com>
2024-01-03 17:51:58 -0300

    Projeto criado

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+
+namespace xadrez_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
