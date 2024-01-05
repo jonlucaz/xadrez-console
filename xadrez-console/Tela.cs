8eb737cfc3a77efb2141c905f01db860af736091
jonlucaz <jonathanlucastri@gmail.com>
2024-01-05 16:46:02 -0300

    fazendo os primeiros testes de movimentacao

--- xadrez-console/Tela.cs
+++ xadrez-console/Tela.cs
@@ -1,4 +1,5 @@
 ﻿using tabuleiro;
+using xadrez;
 
 namespace xadrez_console
 {
@@ -26,9 +27,17 @@ namespace xadrez_console
             Console.WriteLine("  A B C D E F G H");
         }
 
-        public static void imprimirPeca (Peca peca)
+        public static PosicaoXadrez lerPosicaoXadrez()
         {
-            if(peca.cor == Cor.Branca)
+            string s = Console.ReadLine();
+            char coluna = s[0];
+            int linha = int.Parse(s[1] + "");
+            return new PosicaoXadrez(coluna, linha);
+        }
+
+        public static void imprimirPeca(Peca peca)
+        {
+            if (peca.cor == Cor.Branca)
             {
                 Console.Write(peca);
             }
